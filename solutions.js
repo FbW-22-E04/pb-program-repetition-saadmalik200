976010118e011a46e7dfb5e452675085303c2479
Saad Malik <saad_malik200@hotmail.com>
2022-07-12 15:25:09 +0200

    ex3

---
+++ solutions.js
@@ -0,0 +1,25 @@
+// let i = 0;
+// while (i < 15) {
+//   console.log(i);
+//   i++;
+// }
+
+console.log("--------------------------------------");
+
+let j = 1;
+let sum = 0;
+
+while (j <= 20) {
+  sum = sum + j;
+  j++;
+  console.log(sum);
+}
+
+//
+
+let i = 0;
+
+do {
+  console.log("The number is", i);
+  i++;
+} while (i < 20);
